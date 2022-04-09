c391d773975192704b4ddfcecf91238b58504b66
Zhiwei Bao <zhiv.bao@gmail.com>
2022-04-09 14:06:04 -0700

    Add insensitive filter to genres and books

--- catalog/views.py
+++ catalog/views.py
@@ -15,11 +15,15 @@ def index(request):
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
 
+    # Number of books which genre is fiction
+    num_books_genre_fiction = Book.objects.filter(genre__name__icontains='fiction').count()
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_books_genre_fiction': num_books_genre_fiction,
     }
 
     # Render the HTML template index.html with the data in the context variable
